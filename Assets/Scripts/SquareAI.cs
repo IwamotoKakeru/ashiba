50e45056f452c8cd51ca1c42caa7cd9c4e4c3f15
saku.itozono@gmail.com <>
2022-09-07 16:07:46 +0900

    Add AI scrip

---
+++ Assets/Scripts/SquareAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SquareAI : MonoBehaviour
+{
+    // Start is called before the first frame update
+    Square Sq ;
+    public GroundCheck ground,ceiling,wall;
+
+    private bool isGround,isCeiling,isWall;
+
+    void Start()
+    {
+        Sq = gameObject.GetComponent<Square>();
+    }
+
+    void Initialize(){
+        isGround = ground.IsGround();
+        isCeiling = ceiling.IsGround();
+        isWall = wall.IsGround();
+        Sq.Initialize();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Initialize();
+        Sq.Walk(Input.GetAxisRaw("Horizontal"));
+        Sq.jump(Input.GetAxis("Vertical"));
+    }
+}
