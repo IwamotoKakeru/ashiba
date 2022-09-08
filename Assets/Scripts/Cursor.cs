56f2da3e44cf7bd5a9bd0288abb315abde4bfa0e
kganpipi@gmail.com <>
2022-09-08 13:03:51 +0900

    Add Reticle Prefab

---
+++ Assets/Scripts/Cursor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject reticle;
+    private Vector3 mousePosition, target;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousePosition = Input.mousePosition;
+        mousePosition.z = 10;
+        target = Camera.main.ScreenToWorldPoint(mousePosition);
+        transform.position = target;
+        
+
+    }
+}
