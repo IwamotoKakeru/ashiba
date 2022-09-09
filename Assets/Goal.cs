30919fb9bfe59443afd089b130ee631d667294ae
syouta2223505@gmail.com <>
2022-09-09 17:56:07 +0900

    Add Goal

---
+++ Assets/Goal.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+       
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                Debug.Log("ゴール");
+            }
+        
+    }
+}
