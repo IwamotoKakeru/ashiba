a80daacc7bd77f9d07fc36ffa4c4002cba24aee6
syouta2223505@gmail.com <>
2022-09-09 16:46:35 +0900

    Generator can generate

---
+++ Assets/generator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+
+public class generator : MonoBehaviour
+{
+    public GameObject Square;
+    private string cursorTag = "GameController";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(cursorTag))
+        {
+           Instantiate(Square, new Vector2(-6.5f,1.5f), Quaternion.identity);
+        }
+    }
+}
