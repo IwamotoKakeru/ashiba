f867dfc46db0135572dc1d608cdec48b1f6e3998
saku.itozono@gmail.com <>
2022-09-10 16:36:33 +0900

    make Destroyer

---
+++ Assets/Scripts/GoalScore.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.UI;
+
+
+public class GoalScore : MonoBehaviour
+{
+
+    public GameObject score_object = null;
+    public string score_num ; //スコア変数
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        // オブジェクトからTextコンポーネントを取得
+        Text score_text = score_object.GetComponent<Text> ();
+        // テキストの表示を入れ替える
+        score_text.text = score_num;
+    }
+}
