1ba814c2985c82433c8d6b02478166d5231149d4
kganpipi@gmail.com <>
2022-09-10 15:03:42 +0900

    Goal destory player

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -6,6 +6,7 @@ public class Goal : MonoBehaviour
 {
     public AudioClip goal;
     public AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +26,7 @@ public class Goal : MonoBehaviour
             {
                 Debug.Log("ゴール");
                 audioSource.PlayOneShot(goal);
+                Destroy(collision.gameObject);
             }
         
     }
