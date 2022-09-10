34a355de24283fad088a212a395ea67f6c9a98d2
kganpipi@gmail.com <>
2022-09-10 18:00:56 +0900

    Add Goal count system

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -7,16 +7,25 @@ public class Goal : MonoBehaviour
     public AudioClip goal;
     public AudioSource audioSource;
 
+    public int goalNumGetter = 1;
+    private int goalNum = 1;
+
     // Start is called before the first frame update
     void Start()
     {
         audioSource = this.gameObject.GetComponent<AudioSource>();
+        goalNum = goalNumGetter;
+    }
+
+    public int returnGoalNum(){
+        return goalNum;
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+
+    public bool returnGoalFlag(){
+        if(goalNum == 0)    return true;
+        else                return false;
     }
 
     void OnTriggerEnter2D(Collider2D collision)
@@ -24,9 +33,11 @@ public class Goal : MonoBehaviour
        
             if (collision.gameObject.CompareTag("Player"))
             {
-                Debug.Log("ゴール");
-                audioSource.PlayOneShot(goal);
+                goalNum --;
+                Debug.Log("1体ゴール");
                 Destroy(collision.gameObject);
+
+                audioSource.PlayOneShot(goal);
             }
         
     }
