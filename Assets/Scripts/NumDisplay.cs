85f7c2bdd663754b0fea1dc551c54addab832b44
kganpipi@gmail.com <>
2022-09-12 17:50:05 +0900

    Fix NumDisplay

--- Assets/Scripts/NumDisplay.cs
+++ Assets/Scripts/NumDisplay.cs
@@ -7,12 +7,17 @@ public class NumDisplay : MonoBehaviour
     // Start is called before the first frame update
 
     private TextMesh textMesh;
+    private int num;
     void Start()
     {
         textMesh = GetComponent<TextMesh>();
     }
 
-    public void DisplayNum(int num){
+    public void DisplayNum(int numGetter){
+        num = numGetter;
+    }
+
+    void Update(){
         textMesh.text = num.ToString("0");
     }
 
