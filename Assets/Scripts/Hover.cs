59797c2116f5a451829c8e267144f5a037a150c6
IwamotoKakeru <kganpipi@gmail.com>
2024-02-03 04:37:02 +0900

    feat: Hoverスクリプトをアタッチするのみでホバーを実装可能に

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// SpriteRendererを所持しているオブジェクトにこのスクリプトをアタッチするとホバーが実装される
+/// </summary>
+public class Hover : TouchChecker
+{
+    private string cursorTag = "Hover";
+
+    public override string ObjectTag => cursorTag;
+
+    private SpriteRenderer spriteRenderer;
+    private Color hoverColor = new Color(0.8f, 0.8f, 0.8f);
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        if (IsTouching())
+        {
+            spriteRenderer.color = hoverColor;
+        }
+        else
+        {
+            spriteRenderer.color = Color.white;
+        }
+
+    }
+}
--- Assets/Scripts/TouchChecker.cs
+++ Assets/Scripts/TouchChecker.cs
@@ -2,25 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// このスクリプトをアタッチしたオブジェクトがもつコライダーと特定のタグのコライダーとの重なりを完全に検知する
+/// </summary>
 public class TouchChecker : MonoBehaviour
 {
     private string objectTag = "Block";
+
+    /// <summary>
+    /// ObjectTagが返すタグをoverrideすることで他のタグに対応可能
+    /// </summary>
+    public virtual string ObjectTag => objectTag;
     private bool enterFlag, stayFlag, exitFlag;
     private bool isTouch = false;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag(objectTag)) enterFlag = true;
+        if (collision.CompareTag(ObjectTag)) enterFlag = true;
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (collision.CompareTag(objectTag)) stayFlag = true;
+        if (collision.CompareTag(ObjectTag)) stayFlag = true;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.CompareTag(objectTag)) exitFlag = true;
+        if (collision.CompareTag(ObjectTag)) exitFlag = true;
     }
 
     public bool IsTouching()
