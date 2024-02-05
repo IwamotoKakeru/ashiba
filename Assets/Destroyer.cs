2b17cc4675892decbc0249202ebfcb5936cf8c2a
IwamotoKakeru <kganpipi@gmail.com>
2024-02-06 03:32:41 +0900

    fix: 画面外に出た際に削除するオブジェクトをカーソル以外に拡大

--- Assets/Destroyer.cs
+++ Assets/Destroyer.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using Constants;
 using UnityEngine;
 
+/// <summary>
+/// 画面外にでたオブジェクトを削除する
+/// </summary>
 public class Destroyer : MonoBehaviour
 {
     void OnTriggerExit2D(Collider2D collision)
     {
 
-        if (collision.gameObject.CompareTag(Tags.Player))
+        if (!collision.gameObject.CompareTag(Tags.Cursor))
         {
             Destroy(collision.gameObject);
             Debug.Log("消滅");
