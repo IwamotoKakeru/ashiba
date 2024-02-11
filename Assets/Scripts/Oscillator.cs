52dc9b781a2fbc2c3d877d1d0f15653e34ce3934
IwamotoKakeru <kganpipi@gmail.com>
2024-02-11 21:06:45 +0900

    feat: 死体を生成時に振動する機能を実装

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Oscillator oscillator;
+    private float elapsedTime = 0.0f;
+    private float oscillateTime = 3.0f;
+
+    private float angularSpeed = 64.0f;
+    private float maxAmplitude = 0.2f;
+    private float dumpingCoefficient = 2.0f;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        oscillator = GetComponent<Oscillator>();
+        Invoke("StopOscillate", oscillateTime);
+    }
+
+    /// <summary>
+    /// 減衰振動関数, 指数関数的に減衰しながら振動する様子を値で返す
+    /// </summary>
+    /// <param name="_time">時間</param>
+    /// <param name="_angularSpeed">角速度</param>
+    /// <param name="_maxAmplitude">最大振幅</param>
+    /// <param name="_dumpingCoefficient">減衰率</param>
+    /// <returns>振動している座標</returns>
+    double DampedOscillation(double _time, double _angularSpeed, double _maxAmplitude, double _dumpingCoefficient)
+    {
+        return _maxAmplitude * Math.Pow(Math.E, -_time * _dumpingCoefficient) * Math.Cos(_angularSpeed * _time);
+    }
+
+    void StopOscillate()
+    {
+        spriteRenderer.transform.localPosition = new Vector3(0, 0, 0);
+        oscillator.enabled = false;
+    }
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        float xPos = (float)DampedOscillation(elapsedTime, angularSpeed, maxAmplitude, dumpingCoefficient);
+        spriteRenderer.transform.localPosition = new Vector3(xPos, 0, 0);
+    }
+}
