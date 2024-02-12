478c78af9ba1ae9b8bf4ded86799f8f74bcd4a1d
IwamotoKakeru <kganpipi@gmail.com>
2024-02-12 21:05:05 +0900

    feat: オブジェクトプールを実装し、コルーチンを改善()

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Constants;
+using System.Linq;
 
 public class Generator : TouchChecker
 {
     // 検知するタグをカーソルに変更
     public override string ObjectTag => Tags.Cursor;
 
-    public GameObject Square;
+    public GameObject gameObject;
+    private List<GameObject> objectPool = new List<GameObject>();
     private StringDisplay numDisplay;
 
     // インスペクタから取得するため自動実装プロパティのように扱う
@@ -16,20 +18,32 @@ public class Generator : TouchChecker
     private int maxGenerateNum = 1;
     private int generatedNum = 0;
 
-    private float intervalSec = 3.0f;
+    private float intervalSec = 1.0f;
     private Vector3 generateLocalPos = new Vector3(0, -1.5f, 0);
+
+    void InstantiateObjects()
+    {
+        for (int i = 0; i < maxGenerateNum; i++)
+        {
+            GameObject tempObject = Instantiate(gameObject, this.transform.position + generateLocalPos, Quaternion.identity);
+            tempObject.SetActive(false);
+            objectPool.Add(tempObject);
+        }
+    }
 
     void GenerateObject()
     {
+        objectPool.First().SetActive(true);
+        objectPool.RemoveAt(0);
         generatedNum += 1;
         numDisplay.DisplayInt(maxGenerateNum - generatedNum);
-        Instantiate(Square, this.transform.position + generateLocalPos, Quaternion.identity);
     }
 
     // numDisplayに正常に表示させるため、Awakeで数値を取得し、Startで反映させる
     void Awake()
     {
         maxGenerateNum = MaxGenerateNum;
+        InstantiateObjects();
         numDisplay = GetComponentInChildren<StringDisplay>();
     }
     void Start()
@@ -40,21 +54,17 @@ public class Generator : TouchChecker
 
     private IEnumerator GenerateCoroutine()
     {
-        while (true)
+        while (generatedNum < maxGenerateNum)
         {
+            Debug.Log(IsTouching());
             if (IsTouching())
             {
-                if (generatedNum < maxGenerateNum)
-                {
-                    GenerateObject();
-                    yield return new WaitForSeconds(intervalSec);
-                }
+                GenerateObject();
+                yield return new WaitForSeconds(intervalSec);
             }
-            else
-            {
-                // 押されてないなら1F待つ
-                yield return null;
-            }
+            // 1F待つ
+            yield return null;
         }
+        Debug.Log("End coroutine");
     }
 }
