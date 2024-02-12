40f364d955b466c337ebbd4beba1307f4f31abdb
IwamotoKakeru <kganpipi@gmail.com>
2024-02-12 19:06:20 +0900

    feat: ジェネレーターにコルーチンを導入し生成に感覚を取るように変更

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 using Constants;
 
-public class Generator : MonoBehaviour
+public class Generator : TouchChecker
 {
+    // 検知するタグをカーソルに変更
+    public override string ObjectTag => Tags.Cursor;
+
     public GameObject Square;
     private StringDisplay numDisplay;
 
@@ -12,6 +15,16 @@ public class Generator : MonoBehaviour
     public int MaxGenerateNum = 3;
     private int maxGenerateNum = 1;
     private int generatedNum = 0;
+
+    private float intervalSec = 3.0f;
+    private Vector3 generateLocalPos = new Vector3(0, -1.5f, 0);
+
+    void GenerateObject()
+    {
+        generatedNum += 1;
+        numDisplay.DisplayInt(maxGenerateNum - generatedNum);
+        Instantiate(Square, this.transform.position + generateLocalPos, Quaternion.identity);
+    }
 
     // numDisplayに正常に表示させるため、Awakeで数値を取得し、Startで反映させる
     void Awake()
@@ -22,17 +35,25 @@ public class Generator : MonoBehaviour
     void Start()
     {
         numDisplay.DisplayInt(maxGenerateNum);
+        StartCoroutine(GenerateCoroutine());
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    private IEnumerator GenerateCoroutine()
     {
-        if (generatedNum < maxGenerateNum)
+        while (true)
         {
-            if (collision.gameObject.CompareTag(Tags.Cursor))
+            if (IsTouching())
             {
-                Instantiate(Square, this.transform.position + new Vector3(0, -1.5f, 0), Quaternion.identity);
-                generatedNum += 1;
-                numDisplay.DisplayInt(maxGenerateNum - generatedNum);
+                if (generatedNum < maxGenerateNum)
+                {
+                    GenerateObject();
+                    yield return new WaitForSeconds(intervalSec);
+                }
+            }
+            else
+            {
+                // 押されてないなら1F待つ
+                yield return null;
             }
         }
     }
