d060329626c8683caf4dddca3a5a813b381a6b3c
IwamotoKakeru <kganpipi@gmail.com>
2024-02-22 04:55:18 +0900

    feat: GoalGeneralからフェードアウトを呼び出すように変更

--- Assets/Scripts/GoalGeneral.cs
+++ Assets/Scripts/GoalGeneral.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 /// <summary>
@@ -9,17 +11,23 @@ public class GoalGeneral : MonoBehaviour
 {
     private Goal[] goalsScripts;
     private bool goalFlags;
-    private bool instantFlag = false;
+    private bool goaledFlag = false;
     private int sceneNum;
     public GameObject clearLogo;
+    private GameObject fader;
+    private FadeManager fade;
 
     private float intervalTime = 4.0f;
+
 
     void Start()
     {
         sceneNum = SceneManager.GetActiveScene().buildIndex;
         //すべてのゴールスクリプトの取得
+        //重い処理のため要改善
         goalsScripts = FindObjectsOfType<Goal>();
+        fader = GameObject.FindGameObjectWithTag(Constants.Tags.Fader);
+        fade = fader.GetComponent<FadeManager>();
     }
 
     void GoalCheck()
@@ -35,16 +43,18 @@ public class GoalGeneral : MonoBehaviour
     void Update()
     {
         GoalCheck();
-        if (goalFlags && !instantFlag)
+        if (goalFlags && !goaledFlag)
         {
-            Instantiate(clearLogo, new Vector3(0, 0, 0), Quaternion.identity);
-            Invoke("GoToNextScene", intervalTime);
-            instantFlag = true;
+            goaledFlag = true;
+            StartCoroutine(GoToNextScene());
         }
     }
 
-    void GoToNextScene()
+    private IEnumerator GoToNextScene()
     {
-        SceneManager.LoadScene(sceneNum + 1);
+        Instantiate(clearLogo, new Vector3(0, 0, 0), Quaternion.identity);
+        yield return new WaitForSeconds(intervalTime);
+        StartCoroutine(fade.FadeOutCorutine(() => { SceneManager.LoadScene(sceneNum + 1); }));
+        yield return null;
     }
 }
