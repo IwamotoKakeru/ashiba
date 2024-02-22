74cdb76249a32527ba9c05e2db882c9e6f4291f7
IwamotoKakeru <kganpipi@gmail.com>
2024-02-22 20:27:42 +0900

    feat: BGMが途切れないようにBgmManagerを作成

---
+++ Assets/Scripts/BgmManager.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using System;
+
+/// <summary>
+/// 全ステージのBGMを管理する
+/// シングルトンを用いている
+/// </summary>
+public class BgmManager : MonoBehaviour
+{
+    private static bool dontDestroyEnabled = false;
+
+    //シングルトン設定
+    void Awake()
+    {
+        if (dontDestroyEnabled)
+        {
+            Destroy(gameObject);
+        }
+        dontDestroyEnabled = true;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+    public AudioSource StageBgm;
+    public AudioSource EndingBgm;
+
+    void Start()
+    {
+        //シーンが切り替わった時に呼ばれるメソッドを登録
+        SceneManager.activeSceneChanged += OnActiveSceneChanged;
+
+        //エディター上でBGMを流すために使用
+        ChangeBgm(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    void CountinuPlay(AudioSource audioSource)
+    {
+        // 再生されていなければ再生
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    void ChangeBgm(int buildIndex)
+    {
+        if (buildIndex == 0)
+        {
+            //タイトル
+            StageBgm.Stop();
+            EndingBgm.Stop();
+            Debug.Log("Title");
+        }
+        else if (buildIndex == 10)
+        {
+            //エンディング
+            StageBgm.Stop();
+            EndingBgm.Play();
+            Debug.Log("Into Ending");
+        }
+        else
+        {
+            //ステージ
+            CountinuPlay(StageBgm);
+            EndingBgm.Stop();
+            Debug.Log("Into Stage");
+        }
+    }
+
+    //シーンが切り替わった瞬間に呼ばれるメソッド　
+    void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+    {
+        ChangeBgm(nextScene.buildIndex);
+    }
+
+}
