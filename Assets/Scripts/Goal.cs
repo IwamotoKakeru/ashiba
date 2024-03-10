a51b617eb72f1b3bc32a088ed3fd3f5b5d5bdc79
IwamotoKakeru <kganpipi@gmail.com>
2024-03-11 02:12:00 +0900

    feat: goalNumがマイナスになった際に別の音を鳴らすように

--- Assets/Scripts/Goal.cs
+++ Assets/Scripts/Goal.cs
@@ -8,6 +8,7 @@ using Utility;
 public class Goal : MonoBehaviour
 {
     public AudioClip goal;
+    public AudioClip badGoal;
     public AudioSource audioSource;
 
     public int goalNumGetter = 1;// 変数名が規則違反だが、変更するとインスペクタ上の値をすべて変更する必要があるため保留
@@ -32,13 +33,25 @@ public class Goal : MonoBehaviour
         else return false;
     }
 
+    void PlayGoalSound()
+    {
+        if (goalNum >= 0)
+        {
+            audioSource.PlayOneShot(goal);
+        }
+        else
+        {
+            audioSource.PlayOneShot(badGoal);
+        }
+    }
+
     void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag(Tags.Player))
         {
             goalNum--;
             collision.gameObject.SetActive(false);
-            audioSource.PlayOneShot(goal);
+            PlayGoalSound();
             numDisplay.DisplayInt(goalNum);
         }
     }
