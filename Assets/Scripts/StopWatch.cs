b510da419c3396fa840e6f2883df617c8d3806cf
IwamotoKakeru <kganpipi@gmail.com>
2024-05-02 20:30:50 +0900

    feat(StopWatch): テストシーンにストップウォッチを表示

---
+++ Assets/Scripts/StopWatch.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+
+
+/// <summary>
+/// 経過時間を表示する
+/// </summary>
+public class StopWatch : MonoBehaviour
+{
+    System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+    System.TimeSpan timeSpan;
+
+    private StringDisplay stringDisplay;
+    void Start()
+    {
+        sw.Start();
+        stringDisplay = GetComponentInChildren<StringDisplay>();
+    }
+
+    void Update()
+    {
+        stringDisplay.DisplayString(sw.Elapsed.ToString(@"mm\:ss\.ff"));
+    }
+}
