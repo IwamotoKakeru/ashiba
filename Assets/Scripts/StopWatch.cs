6842bd59bfa1b2a7213b3ca6559694b7557f2ba7
IwamotoKakeru <kganpipi@gmail.com>
2024-05-03 15:06:39 +0900

    feat(StopWatch): シングルトン化と簡単なリファクタリング

--- Assets/Scripts/StopWatch.cs
+++ Assets/Scripts/StopWatch.cs
@@ -1,24 +1,69 @@
-﻿using UnityEngine;
-using System;
-
+﻿using UnityEditor.Playables;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Utility;
 
 /// <summary>
-/// 経過時間を表示する
+/// 経過時間を表示するタイマーを管理する
+/// 複数シーンをまたぐためシングルトンを用いる
 /// </summary>
 public class StopWatch : MonoBehaviour
 {
-    System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
-    System.TimeSpan timeSpan;
+    static bool dontDestroyEnabled = false;
 
-    private StringDisplay stringDisplay;
+    System.Diagnostics.Stopwatch stopWatch = new System.Diagnostics.Stopwatch();
+
+    StringDisplay stringDisplay;
+
+    //シングルトン設定
+    void Awake()
+    {
+        if (dontDestroyEnabled)
+        {
+            Destroy(gameObject);
+        }
+        dontDestroyEnabled = true;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
     void Start()
     {
-        sw.Start();
         stringDisplay = GetComponentInChildren<StringDisplay>();
+        //シーンが切り替わった時に呼ばれるメソッドを登録
+        SceneManager.activeSceneChanged += OnActiveSceneChanged;
+    }
+
+    //シーンが切り替わった瞬間に呼ばれるメソッド　
+    void OnActiveSceneChanged(Scene prevScene, Scene nextScene)
+    {
+        ToggleCtrl(nextScene.buildIndex);
+    }
+
+    void ToggleCtrl(int buildIndex)
+    {
+        switch ((SceneIndex)buildIndex)
+        {
+            case SceneIndex.Turtorial:
+                stopWatch.Start();
+                break;
+
+            case SceneIndex.Ending:
+                stopWatch.Stop();
+                break;
+
+            case SceneIndex.Title:
+                stopWatch.Reset();
+                break;
+
+            default:
+                // ステージ
+                break;
+        }
     }
 
     void Update()
     {
-        stringDisplay.DisplayString(sw.Elapsed.ToString(@"mm\:ss\.ff"));
+        // TODO: Update内で呼び出だすとボトルネックになる可能性があるので改善する
+        stringDisplay.DisplayString(stopWatch.Elapsed.ToString(@"mm\:ss\.ff"));
     }
 }
